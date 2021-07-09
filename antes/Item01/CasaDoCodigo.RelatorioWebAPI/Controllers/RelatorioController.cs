fe464ee97b8a91a10f45bf6b966ecb89186b1174
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-07-09 17:54:57 -0300

    Aula 01 - Scaffolding com Web API

---
+++ antes/Item01/CasaDoCodigo.RelatorioWebAPI/Controllers/RelatorioController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CasaDoCodigo.RelatorioWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RelatorioController : ControllerBase
+    {
+        // GET: api/<RelatorioController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Relatório", "Relatório" };
+        }
+
+        // GET api/<RelatorioController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<RelatorioController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<RelatorioController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<RelatorioController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
