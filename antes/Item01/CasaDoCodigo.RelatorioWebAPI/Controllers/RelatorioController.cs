65b24bd4d38e74dd3ba5ad164874b9930dbf8bd6
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-07-10 17:40:20 -0300

    Updated method Get on RelatorioController

--- antes/Item01/CasaDoCodigo.RelatorioWebAPI/Controllers/RelatorioController.cs
+++ antes/Item01/CasaDoCodigo.RelatorioWebAPI/Controllers/RelatorioController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -12,36 +13,52 @@ namespace CasaDoCodigo.RelatorioWebAPI.Controllers
     [ApiController]
     public class RelatorioController : ControllerBase
     {
+
+        private readonly List<string> Relatorio = new List<string>()
+        {
+            "Primeiro pedido",
+            "Segundo pedido"
+        };
+
+
         // GET: api/<RelatorioController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public string Get()
         {
-            return new string[] { "Relatório", "Relatório" };
+            StringBuilder sb = new StringBuilder();
+            
+            foreach (var linha in Relatorio)
+            {
+                sb.AppendLine($"{linha}");
+                
+            }
+            return sb.ToString();
         }
 
-        // GET api/<RelatorioController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+        //// GET api/<RelatorioController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
 
         // POST api/<RelatorioController>
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            Relatorio.Add(value);
         }
+        
+        //// PUT api/<RelatorioController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
 
-        // PUT api/<RelatorioController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<RelatorioController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        //// DELETE api/<RelatorioController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
